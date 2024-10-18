76e47aa02d75f98a00102f9ff005104a0d471d2a
annvoor <annvoor@nuevaschool.org>
2024-10-18 16:31:58 -0700

    added sveltekit dependency, added lesson content (lesson 1)

--- src/routes/data.js
+++ src/routes/data.js
@@ -1,9 +1,18 @@
 export const lessons = [
 	{
 		slug: 'java1',
-		title: "Java Basics 101: Variables  <strong>Start here if you've never programmed before!</strong>",
+		title: "Java Basics 101: Variables",
 		content:
-			'<p>yibbity bibbity box is variable.</p><p>do math.</p>'
+			`<p>(If you’re familiar with programming, you can skim this section to get an idea of how Java syntax works)</p>
+			<h3>What is a variable?</h3>
+			<p>Variables are like a labeled box. The box has a name but what’s inside of it can always be changed. For example I have a box named “x”: what's within box x could be a number, it could be a name, it could be anything.</p>
+			<p>For now let's just say the box “x” has the number two within it:</p>
+			<code>x = 2;</code>
+			<p>If we want to change what 'x' is we could say on a later line of code that 'x = 4' for example:</p>
+			<code>x = 2;</code>
+			<br>
+			<code>x = 4;</code>
+			`
 	},
 
 	{
