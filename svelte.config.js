fac71926a9fef89557bf7a141ac42ab5c31742d7
annvoor <annvoor@nuevaschool.org>
2024-10-18 17:07:36 -0700

    added vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,4 +11,12 @@ const config = {
 	}
 };
 
-export default config;
+export default {
+	kit: {
+		adapter: adapter({
+			// see below for options that can be set here
+		})
+	}
+};
+
+
